f51870d59e0a42ed31d843217dc7af8298db51a2
Goudar <113749395+karthikgoudar@users.noreply.github.com>
2023-03-03 16:31:30 +0530

    Add files via upload

---
+++ Trees/Tree Level-1/SIzeOfBinaryTree.py
@@ -0,0 +1,40 @@
+'''
+Find the size of the binary tree
+
+Time Complexity  : O(n)
+Space Complexity : O(n)
+
+Author : Kartik Goudar
+Date   : 3 Mar, 2023
+'''
+
+'''
+Calculate the size of left and right subtrees respectively
+Add 1 (current node) and return to it's parent
+'''
+
+def findSize(root):
+    if not root:
+        return 0
+    return findSize(root.left) + findSize(root.right) + 1
+
+
+# Using Level order traversal
+
+def findSize1(root):
+    if not root:
+        return 0
+    q =  []
+    q.append(root)
+    node = None
+    count = 0
+    while q:
+        node = q.pop(0)
+        count += 1
+        if node.left:
+            q.append(node.left)
+        if node.right:
+            q.append(node.right)
+
+    return count
+
